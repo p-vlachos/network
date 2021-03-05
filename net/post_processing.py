f9b22e93f59bb11e0ca9903a33c123fd31d084ac
Jan Marker <marker@fias.uni-frankfurt.de>
2021-03-05 12:17:10 +0100

    Reorder post-processing steps

--- net/post_processing.py
+++ net/post_processing.py
@@ -223,6 +223,10 @@ def post_process_scl_rec(tr):
 
 def post_process(tr):
 
+    branching_ratio_figure(f'builds/{tr.v_idx:04d}')
+    post_process_stdp_rec(tr)
+    post_process_scl_rec(tr)
+
     if tr.turnover_rec:
 
         if tr.strct_active:
@@ -236,11 +240,8 @@ def post_process(tr):
             post_process_per_neuron_turnover_counts(tr, 'EI')
 
         
-    srvprb_EE_figure('builds/%.4d'%(tr.v_idx))
-    srvprb_EI_figure('builds/%.4d'%(tr.v_idx))
-    srvprb_all_figure('builds/%.4d'%(tr.v_idx))
+        srvprb_EE_figure('builds/%.4d'%(tr.v_idx))
+        srvprb_EI_figure('builds/%.4d'%(tr.v_idx))
+        srvprb_all_figure('builds/%.4d'%(tr.v_idx))
 
-    branching_ratio_figure(f'builds/{tr.v_idx:04d}')
 
-    post_process_stdp_rec(tr)
-    post_process_scl_rec(tr)
